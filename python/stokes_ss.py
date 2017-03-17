e9c7443b6d3d68c4dd0c74b5ef116e145857c396
daniels <behr@mpi-magdeburg.mpg.de>
2017-03-17 12:30:38 +0100

    weiter

--- python/stokes_ss.py
+++ python/stokes_ss.py
@@ -1,45 +1,61 @@
 import numpy as np
-
 import scipy.io
 import scipy.sparse as sps
 import scipy.sparse.linalg as spsla
 
-N = 5812
-Re = 80
+# hard coded paths and dictionary for data
+NVdict          = {1: 5812, 2: 9356,  3: 19468}
+savedmatsstr    = lambda NV: '../data/cylinderwake__mats_NV{1}_Re{0}.mat'.format(1,NV)
+visujsonstr     = lambda N : '../data/visualization_cylinderwake_N{0}.jsn'.format(N)
 
-savedmatsstr = '../data/cylinderwake__mats_NV{1}_Re{0}.mat'.\
-    format(1, N)
-print 'Re = ', Re
-print 'NV = ', N
 
-# get the coefficients
-mats = scipy.io.loadmat(savedmatsstr)
-A = 1./Re*mats['A']
-M = mats['M']
-J = mats['J']
-fv = mats['fv'] + 1./Re*mats['fv_diff']
-fp = mats['fp'] + mats['fp_div']
-NV, NP = fv.shape[0], fp.shape[0]
+# setup parameters
+Re          = 80
+npicardstps = 5
+N           = 1
+NV          = NVdict[N]
 
+
+# visualisation files
+pfile = 'p__cylinderwake_stst_Re{0}_NV{1}.vtu'.format(Re, NV)
+vfile = 'v__cylinderwake_stst_Re{0}_NV{1}.vtu'.format(Re, NV)
+
+
+# print reynolds number and discretization lvl
+print 'Re = {0}'.format(Re)
+print 'NV = {0}'.format(NV)
+print '\n'
+
+
+# load the coefficients matrices
+mats    = scipy.io.loadmat(savedmatsstr(NV))
+M       = mats['M']
+A       = 1./Re*mats['A']
+J       = mats['J']
+hmat    = mats['H']
+fv      = mats['fv'] + 1./Re*mats['fv_diff']
+fp      = mats['fp'] + mats['fp_div']
+NV, NP  = fv.shape[0], fp.shape[0]
+
+
+# solve stokes equation
 stksrhs = np.vstack([fv, fp])
-stksmat = sps.vstack([sps.hstack([A, -J.T]),
-                      sps.hstack([J, sps.csc_matrix((NP, NP))])])
-stksvp = spsla.spsolve(stksmat, stksrhs).reshape((NV+NP, 1))
-stksv = stksvp[:NV].reshape((NV, 1))
-stksp = stksvp[NV:].reshape((NP, 1))
+stksmat = sps.vstack([sps.hstack([A, -J.T]),sps.hstack([J, sps.csc_matrix((NP, NP))])]).tocsc()
+stksvp  = spsla.spsolve(stksmat, stksrhs).reshape((NV+NP, 1))
+stksv   = stksvp[:NV].reshape((NV, 1))
+stksp   = stksvp[NV:].reshape((NP, 1))
 
-resstksmom = A*stksv - J.T*stksp - fv
-resconti = J*stksv - fp
-print 'The Stokes momentum eq residual: ', np.linalg.norm(resstksmom)
-print 'The conti residual: ', np.linalg.norm(resconti)
 
-try:
-    savedmatsstr = '../data/cylinderwake__mats_NV{1}_Re{0}.mat'.\
-        format(1, N)
-    mats = scipy.io.loadmat(savedmatsstr)
-    vssstks = mats['v_ss_stokes']
-    pssstks = mats['p_ss_stokes']
-    print 'Difference of v solutions: ', np.linalg.norm(stksv - vssstks)
-    print 'Difference of p solutions: ', np.linalg.norm(stksp - pssstks)
-except IOError:
-    print 'No data for Re={0}, N={1}'.format(Re, N)
+# print results
+resstksmom  = A*stksv - J.T*stksp - fv
+resconti    = J*stksv - fp
+print '*** Done ***'
+print 'The Stokes momentum eq residual: {0:e}'.format(np.linalg.norm(resstksmom))
+print 'The conti residual:              {0:e}'.format(np.linalg.norm(resconti))
+
+vssstks = mats['v_ss_stokes']
+pssstks = mats['p_ss_stokes']
+ 
+print 'Difference of v solutions:       {0:e}'.format(np.linalg.norm(stksv - vssstks))
+print 'Difference of p solutions:       {0:e}'.format(np.linalg.norm(stksp - pssstks))
+
