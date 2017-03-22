fa5f165e768a380de3e820ddb0c9def974ff4b57
daniels <behr@mpi-magdeburg.mpg.de>
2017-03-22 13:04:59 +0100

    weiter

--- matlab/cylinderwake_tdp_pout_vout.m
+++ matlab/cylinderwake_tdp_pout_vout.m
@@ -27,17 +27,17 @@ trange      = linspace(t0, tE, Nts+1);
 
 
 %% parameters for results, directories
-rdir        = 'results/'
+rdir        = 'results/';
 vfileprfx   = sprintf('v_tdpcyl_NV%d_Re%d_tE%e_Nts%d',NV, Re, tE, Nts);
-pfileprfx   = sprintf('p_tdpcyl_NV%d_Re%d_tE%e_Nts%d'.NV, Re, tE, Nts);
-poutlist    = {};
-voutlist    = {};
+pfileprfx   = sprintf('p_tdpcyl_NV%d_Re%d_tE%e_Nts%d',NV, Re, tE, Nts);
+voutlist    = cell(1,length(trange));
+poutlist    = cell(1,length(trange));
 vfile       = @(t) sprintf('%s%s__t%e.vtu',rdir,vfileprfx,t);
 pfile       = @(t) sprintf('%s%s__t%e.vtu',rdir,pfileprfx,t);
 vfilelist   = {vfile(trange(1))};
 pfilelist   = {pfile(trange(1))};
-ptikzfile   = sprintf('tikz/p_nsequadtens-N%d-tE%e-Nts%d', N, tE, Nts);
 vtikzfile   = sprintf('tikz/v_nsequadtens-N%d-tE%e-Nts%d', N, tE, Nts);
+ptikzfile   = sprintf('tikz/p_nsequadtens-N%d-tE%e-Nts%d', N, tE, Nts);
 
 
 %% print reynolds number and discretization lvl
@@ -56,72 +56,67 @@ M       = mats.M;
 A       = 1./Re*mats.A + mats.L1 + mats.L2;
 J       = mats.J;
 hmat    = mats.H;
-Brob    = mats.Brob;
 fv      = mats.fv + 1./Re*mats.fv_diff + mats.fv_conv;
 fp      = mats.fp + mats.fp_div;
+pcmat   = mats.Cp;
+vcmat   = mats.Cv;
 NV      = size(fv,1);
 NP      = size(fp,1);
 
-# load the coefficients matrices
-mats    = scipy.io.loadmat(savedmatsstr(NV))
-M       = mats['M']
-A       = 1./Re*mats['A'] + mats['L1'] + mats['L2']
-J       = mats['J']
-hmat    = mats['H']
-fv      = mats['fv'] + 1./Re*mats['fv_diff'] + mats['fv_conv']
-fp      = mats['fp'] + mats['fp_div']
-pcmat   = mats['Cp']
-vcmat   = mats['Cv']
-NV, NP  = fv.shape[0], fp.shape[0]
+
+%% factorization of system matrix
+fprintf('computing LU once...\n');
+sysmati                     = sparse([[M+DT*A, -J'];[J, sparse(NP, NP)]]);
+[sysL,sysU,sysP,sysQ,sysR]  = lu(sysmati);            
+sysmati                     = @(x) sysQ * (sysU \ (sysL \ (sysP * (sysR \ x))));
 
 
-# factorization of system matrix
-print 'computing LU once...'
-sysmat  = sps.vstack([sps.hstack([M+DT*A, -J.T]), sps.hstack([J, sps.csc_matrix((NP, NP))])]).tocsc()
-sysmati = spsla.factorized(sysmat)
+%% compute stokes solution as initial value
+fprintf('computing Stokes solution to be used as initial value...\n');
+fvstks  = mats.fv + 1./Re*mats.fv_diff;
+Astks   = 1/Re*mats.A;
+stksrhs = [fvstks; fp];
+stksmat = sparse([[Astks, -J'];[J, sparse(NP, NP)]]);
+stksvp  = stksmat\stksrhs;
+stksv   = stksvp(1:NV);
+stksp   = stksvp(NV+1:end);
 
 
-# compute stokes solution as initial value
-print 'computing Stokes solution to be used as initial value...'
-fvstks  = mats['fv'] + 1./Re*mats['fv_diff']
-Astks   = 1./Re*mats['A']
-stksrhs = np.vstack([fvstks, fp])
-stksmat = sps.vstack([sps.hstack([Astks, -J.T]),sps.hstack([J, sps.csc_matrix((NP, NP))])]).tocsc()
-stksvp  = spsla.spsolve(stksmat, stksrhs).reshape((NV+NP, 1))
-stksv   = stksvp[:NV].reshape((NV, 1))
-stksp   = stksvp[NV:].reshape((NP, 1))
-
-# Preparing for the output
-vu.writevp_paraview(velvec=stksv, pvec=stksp, vfile=vfile(trange[0]), pfile=pfile(trange[0]), strtojson=visujsonstr(NV))
+%% Preparing for the output
+writevp_paraview(stksv, stksp, visujsonstr(NV), vfile(trange(1)), pfile(trange(1)));
 
 
-# time stepping
-print 'doing the time loop...'
-old_v = stksv
+%% time stepping
+fprintf('doing the time loop...\n');
+old_v = stksv;
 
-for k, t in enumerate(trange):
-    crhsv   = M*old_v + DT*(fv - ctu.eva_quadterm(hmat, old_v))
-    crhs    = np.vstack([crhsv, fp])
-    vp_new  = np.atleast_2d(sysmati(crhs.flatten())).T
-    old_v   = vp_new[:NV]
-    p       = vp_new[NV:]
+for k = 1:length(trange)
+    crhsv   = M*old_v + DT*(fv - eva_quadterm(hmat, old_v));
+    crhs    = [crhsv; fp];
+    vp_new  = sysmati(crhs);
+    old_v   = vp_new(1:NV);
+    p       = vp_new(NV+1:end);
+    
+    poutlist{ k } = pcmat*p;
+    voutlist{ k } = vcmat*old_v;
+    if mod(k, round(Nts/10)) == 0
+        fprintf('timestep %4d/%d, t=%f, |v|=%e\n',k, Nts, trange(k), norm(old_v));
+        writevp_paraview(old_v, p, visujsonstr(NV), vfile(trange(k)), pfile(trange(k)));
+        pfilelist{ length(pfilelist) + 1 } = pfile(trange(k));
+        vfilelist{ length(vfilelist) + 1 } = vfile(trange(k));
+    end
+    
+end
 
-    poutlist.append((pcmat*p)[0][0])
-    voutlist.append((vcmat*old_v).flatten())
-    if np.mod(k, round(Nts/10)) == 0:
-        print('timestep {0:4d}/{1}, t={2:f}, |v|={3:e}'.format(k, Nts, t, np.linalg.norm(old_v)))
-        vu.writevp_paraview(velvec=old_v, pvec=p, vfile=vfile(t), pfile=pfile(t),strtojson=visujsonstr(NV))
-        vfilelist.append(vfile(t))
-        pfilelist.append(pfile(t))
+
+%% save collection to pvd file
+collect_vtu_files(vfilelist, strcat(vfileprfx,'.pvd'));
+collect_vtu_files(pfilelist, strcat(pfileprfx,'.pvd'));
 
 
-# save collection to pvd file
-vu.collect_vtu_files(vfilelist, vfileprfx+'.pvd')
-vu.collect_vtu_files(pfilelist, pfileprfx+'.pvd')
-
-# write to tikz file
-vu.plot_prs_outp(outsig=poutlist, tmesh=trange, tikzfile=ptikzfile)
-vu.plot_prs_outp(outsig=voutlist, tmesh=trange, tikzfile=vtikzfile)
+%% write to tikz file
+plot_prs_outp(voutlist, trange, vtikzfile);
+plot_prs_outp(poutlist, trange, ptikzfile);
 
 
 
